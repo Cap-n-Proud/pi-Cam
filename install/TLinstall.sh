f0f01ad5dd4b44373aa6d9f14eff21211696aa19
pfnegrini <pfnegrini@gmail.com>
2015-01-01 17:41:31 +0100

    Update TLinstall.sh

--- install/TLinstall.sh
+++ install/TLinstall.sh
@@ -31,5 +31,7 @@ echo -e "***** Setting up  TLserver *****"
 sudo cp TLserver /etc/init.d/TLserver
 sudo chmod 0755 /etc/init.d/TLserver
 sudo update-rc.d TLserver defaults
+mkdir /home/pi/Documents/TL
+sudo chmod 0755 /home/pi/Documents/TL
 
 exit 0
