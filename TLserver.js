011fd55eb875105e17621225fd0716e3b85ffed2
pfnegrini <pfnegrini@gmail.com>
2015-02-01 11:56:19 +0100

    Code beautified http://jsbeautifier.org/

--- TLserver.js
+++ TLserver.js
@@ -1,9 +1,8 @@
 // sudo udevadm control --reload-rules
 // to refresh the port allocation
-
 var events = require('events');
 var eventEmitter = new events.EventEmitter();
-var TLFolder="/home/pi/Documents/TL/"
+var TLFolder = "/home/pi/Documents/TL/"
 
 
 var serPort = "/dev/ttyACM0";
@@ -12,8 +11,8 @@ var serverPort = 999;
 
 var LogR = 0;
 var TelemetryFN = "";
-var prevTel="";
-var prevPitch="";
+var prevTel = "";
+var prevPitch = "";
 
 var SEPARATOR = ","
 var version = "0.2";
@@ -40,135 +39,150 @@ var TLInterval = 10000;
 app.use(express.static(__dirname + '/public'));
 // Routers
 {
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-  res.end;
-});
+    app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/public/index.html');
+        res.end;
+    });
 
-app.get('/vj', function(req, res) {
-  res.sendFile(__dirname + '/public/robotj.html');
-  res.end;
-});
+    app.get('/vj', function(req, res) {
+        res.sendFile(__dirname + '/public/robotj.html');
+        res.end;
+    });
 
-app.get('/REBOOT', function(req, res) {
-  var postData = req.url;
-  function puts(error, stdout, stderr) { sys.puts(stdout) }
-  exec('sudo reboot now');
-  sockets.emit('Info', "Rebooting")
-  //console.log(postData);
-  res.end;
-});
+    app.get('/REBOOT', function(req, res) {
+        var postData = req.url;
+
+        function puts(error, stdout, stderr) {
+            sys.puts(stdout)
+        }
+        exec('sudo reboot now');
+        sockets.emit('Info', "Rebooting")
+            //console.log(postData);
+        res.end;
+    });
 
 
 }
 
 // Logging middleware
 
-var mkdirSync = function (path) {
-  try {
-    fs.mkdirSync(path);
-  } catch(e) {
-    if ( e.code != 'EEXIST' ) throw e;
-  }
+var mkdirSync = function(path) {
+    try {
+        fs.mkdirSync(path);
+    } catch (e) {
+        if (e.code != 'EEXIST') throw e;
+    }
 }
 
-io.on('connection', function(socket){
-  var myDate = new Date();
-  socket.emit('Info', 'Connected ' + myDate.getHours() + ':' +   myDate.getMinutes() + ':' +   myDate.getSeconds());
-  console.log('New socket.io connection - id: %s', socket.id);
-  function puts(error, stdout, stderr) { sys.puts(stdout) }
-  exec('sudo /home/pi/pi-blaster/pi-blaster 4 17');
+io.on('connection', function(socket) {
+    var myDate = new Date();
+    socket.emit('Info', 'Connected ' + myDate.getHours() + ':' + myDate.getMinutes() + ':' + myDate.getSeconds());
+    console.log('New socket.io connection - id: %s', socket.id);
+
+    function puts(error, stdout, stderr) {
+        sys.puts(stdout)
+    }
+    exec('sudo /home/pi/pi-blaster/pi-blaster 4 17');
 
 
 
-  socket.on('Video', function(Video){
-   socket.emit('CMD', Video);
-    console.log(Video);
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    exec('sudo bash ./scripts/' + Video, puts);
-      });
+    socket.on('Video', function(Video) {
+        socket.emit('CMD', Video);
+        console.log(Video);
+
+        function puts(error, stdout, stderr) {
+            sys.puts(stdout)
+        }
+        exec('sudo bash ./scripts/' + Video, puts);
+    });
 
 
-  socket.on('REBOOT', function(){
-    function puts(error, stdout, stderr) { sys.puts(stdout) }
-    exec('sudo reboot now');
-    sockets.emit('Info', "Rebooting")
-  });
+    socket.on('REBOOT', function() {
+        function puts(error, stdout, stderr) {
+            sys.puts(stdout)
+        }
+        exec('sudo reboot now');
+        sockets.emit('Info', "Rebooting")
+    });
 
-  socket.on('move', function(dX, dY){
-    //console.log('event: ', dX, dY);
-  //Need a value -100, 100
-  piblaster.setPwm(17, (rescale(parseFloat(dX),-100,100,0.07,0.20)));
-  piblaster.setPwm(4, (rescale(parseFloat(dY),-100,100,0.06,0.19)));
+    socket.on('move', function(dX, dY) {
+        //console.log('event: ', dX, dY);
+        //Need a value -100, 100
+        piblaster.setPwm(17, (rescale(parseFloat(dX), -100, 100, 0.07, 0.20)));
+        piblaster.setPwm(4, (rescale(parseFloat(dY), -100, 100, 0.06, 0.19)));
 
-  //console.log(parseFloat(dX));
-  //console.log(parseFloat(dY));
+        //console.log(parseFloat(dX));
+        //console.log(parseFloat(dY));
 
-  //console.log(rescale(parseFloat(dY),-50,50,0.06,0.15));
-  //console.log(rescale(parseFloat(dX),-100,100,0.07,0.20));
-  });
+        //console.log(rescale(parseFloat(dY),-50,50,0.06,0.15));
+        //console.log(rescale(parseFloat(dX),-100,100,0.07,0.20));
+    });
 
-  socket.on('TLInterval', function(T){
-    TLInterval = T;
-    socket.emit('Info', 'timelapse set to ' + T/1000 + 's')
-  });
+    socket.on('TLInterval', function(T) {
+        TLInterval = T;
+        socket.emit('Info', 'timelapse set to ' + T / 1000 + 's')
+    });
 
-  function TLF() {
-     var MyDate = new Date();
-     var MyTimeStamp;
-    MyTimeStamp = ('0' + MyDate.getHours()).slice(-2) + '-' + ('0' + (MyDate.getMinutes())).slice(-2) + '-' + ('0' + (MyDate.getSeconds())).slice(-2) ;
+    function TLF() {
+        var MyDate = new Date();
+        var MyTimeStamp;
+        MyTimeStamp = ('0' + MyDate.getHours()).slice(-2) + '-' + ('0' + (MyDate.getMinutes())).slice(-2) + '-' + ('0' + (MyDate.getSeconds())).slice(-2);
 
-	 function puts(error, stdout, stderr) { sys.puts(stdout) }
-	  exec('sudo raspistill -w ' + imgWidth + ' -h ' + imgHeight + ' -o ' + TLfolderName + '/' + TLfileName + '_' + MyTimeStamp +  '.jpg  -sh 40 -awb auto -mm average -v');
+        function puts(error, stdout, stderr) {
+            sys.puts(stdout)
+        }
+        exec('sudo raspistill -w ' + imgWidth + ' -h ' + imgHeight + ' -o ' + TLfolderName + '/' + TLfileName + '_' + MyTimeStamp + '.jpg  -sh 40 -awb auto -mm average -v');
         //console.log('sudo raspistill -w ' + imgWidth + ' -h ' + imgHeight + ' -o ' + TLfolderName + '/' + TLfileName + '_' + MyTimeStamp +  '.jpg  -sh 40 -awb auto -mm average -v');
-	socket.emit('Info', TLfileName + '_' + MyTimeStamp +  '.jpg');
+        socket.emit('Info', TLfileName + '_' + MyTimeStamp + '.jpg');
 
     }
 
-  function rescale(x, in_min, in_max, out_min, out_max){
-    var output;
-    output = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    //console.log(output);
-    if (output < out_min) {
-      output = out_min;
-    } else if (output > out_max) {
-      output = out_max;
-    } else {
-      //Nothing
+    function rescale(x, in_min, in_max, out_min, out_max) {
+        var output;
+        output = (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        //console.log(output);
+        if (output < out_min) {
+            output = out_min;
+        } else if (output > out_max) {
+            output = out_max;
+        } else {
+            //Nothing
+        }
+
+        return output
+
     }
 
-    return output
+    var myVar = "";
+    socket.on('TLStart', function() {
+        // function puts(error, stdout, stderr) { sys.puts(stdout) }
+        var MyDate = new Date();
+        var MyDateString;
+        var MyTimeStamp;
+        MyDateString = ('0' + MyDate.getFullYear()).slice(-2) + '-' + ('0' + (MyDate.getMonth() + 1)).slice(-2) + '-' + ('0' + (MyDate.getUTCDate())).slice(-2);
+        MyTimeStamp = ('0' + MyDate.getHours()).slice(-2) + '-' + ('0' + (MyDate.getMinutes())).slice(-2) + '-' + ('0' + (MyDate.getSeconds())).slice(-2);
 
-  }
+        TLfolderName = TLFolder + 'TL_' + MyDateString + '_' + MyTimeStamp;
+        TLfileName = 'TL_' + MyDateString;
+        fs.mkdir(TLfolderName);
+        socket.emit('Folder', TLfolderName);
+        //console.log(TLfolderName);
+        myVar = setInterval(function() {
+            TLF()
+        }, TLInterval);
+    });
 
-  var myVar = "";
-  socket.on('TLStart', function(){
-   // function puts(error, stdout, stderr) { sys.puts(stdout) }
-    var MyDate = new Date();
-    var MyDateString;
-    var MyTimeStamp;
-    MyDateString = ('0' + MyDate.getFullYear()).slice(-2) + '-' + ('0' + (MyDate.getMonth()+1)).slice(-2) + '-' + ('0' + (MyDate.getUTCDate())).slice(-2) ;
-    MyTimeStamp = ('0' + MyDate.getHours()).slice(-2) + '-' + ('0' + (MyDate.getMinutes())).slice(-2) + '-' + ('0' + (MyDate.getSeconds())).slice(-2) ;
+    socket.on('TLStop', function() {
+        clearInterval(myVar);
+        socket.emit('Info', 'TL stopped');
 
-    TLfolderName = TLFolder + 'TL_' + MyDateString + '_' + MyTimeStamp;
-    TLfileName = 'TL_' + MyDateString;
-    fs.mkdir(TLfolderName);
-    socket.emit('Folder', TLfolderName);
-    //console.log(TLfolderName);
-    myVar = setInterval(function(){TLF()}, TLInterval);
-  });
-
-   socket.on('TLStop', function(){
-    clearInterval(myVar);
-      socket.emit('Info', 'TL stopped');
-
-  });
+    });
 
 });
 
 
-http.listen(serverPort, function(){
-console.log('listening on *: ', serverPort);//
+http.listen(serverPort, function() {
+    console.log('listening on *: ', serverPort); //
 
 
 });
