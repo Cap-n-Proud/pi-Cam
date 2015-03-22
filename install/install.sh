0e9e375733e478cd9f93965ea78d09c98b9d9988
pfnegrini <pfnegrini@gmail.com>
2015-03-22 16:40:15 +0100

    BugFix

---
+++ install/install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# to download the latest repository: 
+#git clone https://github.com/pfnegrini/time-lapse-server.git /home/pi/time-lapse-server
+
+asd() {
+cat <<"EOT"
+
+
+
+
+                         .^.
+                         /   \
+                        /     \
+                *******/       \*******
+           ***** *****/         \***** *****
+       ***** ********/           \******** *****
+      *** **********/             \********** ***
+       ***** ******/               \****** *****
+           ***** */        _**_     \* *****
+                */      _-******\    \*
+                /    _-" *****   "\   \
+                \__-"              "\_/
+
+
+
+EOT
+}
+
+asd
+
+echo -n "Want to confgure RaspberryPi? [Y/N]"
+read BUILDUP
+
+git clone https://github.com/sarfata/pi-blaster.git /home/pi/pi-blaster
+
+
+if [ "$BUILDUP" == "Y" ]
+then
+  git clone https://github.com/pfnegrini/BuildUP.git /home/pi/BuildUP
+  sudo bash /home/pi/BuildUP/RPi-init.sh
+fi
+
+echo -e "***** Setting up  TLserver *****"
+sudo cp piCamServer /etc/init.d/piCamServer
+sudo chmod 0755 /etc/init.d/piCamServer
+sudo update-rc.d piCamServer defaults
+
+mkdir /home/pi/Documents
+sudo chmod 0755 /home/pi/Documents
+
+mkdir /home/pi/Documents/TL
+sudo chmod 0755 /home/pi/Documents/TL
+
+mkdir /home/pi/Documents/SN
+sudo chmod 0755 /home/pi/Documents/SN
+
+#Install PiBlaster
+echo -e "***** Installing PiBLaster *****"
+sudo apt-get install -y autoconf
+git clone https://github.com/sarfata/pi-blaster.git /home/pi/pi-blaster
+
+cd /home/pi/pi-blaster
+./autogen.sh
+./configure
+sudo make
+sudo make install
+sudo npm install -g pi-blaster.js
+
+exit 0
